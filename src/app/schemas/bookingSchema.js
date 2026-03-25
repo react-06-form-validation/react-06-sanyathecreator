de5560ccde0c68e6191f9802bc4d6f911e295db2
Sanya, The Creator <olexxxandr.romaniuk@gmail.com>
2026-03-25 18:51:48 +0100

    add custome error messages

--- src/app/schemas/bookingSchema.js
+++ src/app/schemas/bookingSchema.js
@@ -3,11 +3,14 @@ import { z } from "zod";
 // Starter schema for the task. Implement full validation rules from README.
 export const createBookingSchema = (timeSlots) =>
   z.object({
-    bookerName: z.string().min(2),
-    bookerEmail: z.email().optional(),
-    eventName: z.string().min(2),
-    eventDate: z.coerce.date().min(new Date(), { message: "Must be a future date" }),
-    numberOfGuests: z.coerce.number().min(2).max(10),
-    timeSlot: z.enum(timeSlots),
-    eventLink: z.url(),
+    bookerName: z.string().min(2, { message: "Booker name must be at least 2 characters long" }),
+    bookerEmail: z.preprocess(
+      (val) => (val === "" ? undefined : val),
+      z.email().optional()
+    ),
+    eventName: z.string().min(2, { message: "Event name must be at least 2 characters long" }),
+    eventDate: z.coerce.date().min(new Date(), { message: "Event date must be in the future" }),
+    numberOfGuests: z.coerce.number().min(1).max(10, { message: "Number of Guests must be less than or equal to 10" }),
+    timeSlot: z.enum(timeSlots, { message: "Selected time slot is unavailable" }),
+    eventLink: z.url({ message: "Invalid URL. Please enter a valid event link" }),
   });
